e54e8848b4a361f2264afd0fd66a5bb0861def14
Aiden <12aduffy@gmail.com>
2019-12-02 21:14:05 -0500

    commit 3
    added Graph, Node, and Test (redundant to keep the last one in) files
    
    updated the Main to include functionality for displaying the results of
    the functions in Graph
    
    assignment complete barring any bug fixes

---
+++ SE2205-GUI_Assignment/src/sample/Graph.java
@@ -0,0 +1,152 @@
+package sample;
+
+import java.util.*;
+
+public class Graph {
+
+	private String dfs = ""; // required to deal with javafx
+	private HashMap<Node, LinkedList<Node>> adjacencyMap;
+
+	public Graph() {
+
+		adjacencyMap = new HashMap<>();
+	}
+
+	public void addEdgeHelper(Node a, Node b) {
+
+		LinkedList<Node> tmp = adjacencyMap.get(a);
+
+		if (tmp != null) {
+			tmp.remove(b);
+		} else
+			tmp = new LinkedList<>();
+		tmp.add(b);
+		adjacencyMap.put(a, tmp);
+	}
+
+	public void addEdge(Node source, Node destination) {
+
+		if (!adjacencyMap.keySet().contains(source))
+			adjacencyMap.put(source, null);
+
+		if (!adjacencyMap.keySet().contains(destination))
+			adjacencyMap.put(destination, null);
+
+		addEdgeHelper(source, destination);
+		addEdgeHelper(destination, source);
+
+	}
+
+	public boolean hasEdge(Node source, Node destination) {
+
+		return adjacencyMap.containsKey(source) && adjacencyMap.get(source).contains(destination);
+	}
+
+	public void resetNodesVisited() {
+		for (Node n : adjacencyMap.keySet()) {
+			n.unvisit();
+		}
+
+	}
+
+	public String BFS(Node node) {
+
+		// string return, required for GUI component
+		String bfs = "";
+		
+		// code to account for the chance that the node in the argument does not exist
+		// in the graph
+		if (node == null)
+			return bfs;
+
+		// create a queue
+		LinkedList<Node> queue = new LinkedList<>(); // use a linked list as the queue
+		queue.add(node); // add the first node to the queue
+
+		// loop to remove first element in queue while the queue has a first element
+		// available to move
+		while (!queue.isEmpty()) {
+
+			Node curFirst = queue.removeFirst(); // removes the first element in the queue
+
+			// code to handle the event that a visited node is revisited
+			if (curFirst.isVisited())
+				continue; // skips to the next available node that has not yet been visited
+
+			// a visited node is marked as such
+			curFirst.visit();
+			bfs += (curFirst.name + " "); // adds the node being visited to the output
+
+			// create a list of all the current node's neighbouring nodes
+			LinkedList<Node> allNeighbours = adjacencyMap.get(curFirst);
+
+			// check to see if the list of neighboring nodes is empty
+			if (allNeighbours == null)
+				continue; // if there are no neighbors, the search continues
+
+			for (Node neighbour : allNeighbours) {
+
+				// add all unvisited neighboring nodes to the queue (list of nodes to visit)
+				if (!neighbour.isVisited()) {
+					queue.add(neighbour);
+				}
+			}
+		}
+		
+		bfs += "\n"; // clears the row for the next node
+
+		return bfs; 
+	}
+
+	public void DFS(Node node) {
+
+		// visits the first node, arbitrarily chosen
+		node.visit();
+		dfs += (node.name + " "); // adds the name of the visited nodes, required for GUI portion
+
+		// creates a queue using a linked list for all the neighboring nodes of a given
+		// node
+		LinkedList<Node> allNeighbours = adjacencyMap.get(node);
+		if (allNeighbours == null)
+			return; // if there are no neighboring nodes, return up a level, as there are no more
+					// possible nodes to visit
+
+		// search through all neighbouring nodes to find one that has not been visited
+		for (Node neighbour : allNeighbours) {
+			if (!neighbour.isVisited()) // checks to see if this node has been visited
+				DFS(neighbour); // recursively check the unvisited neighbours
+		}
+
+	}
+
+	public String DFSforGUI(Node node) {
+
+		this.DFS(node); // takes in the dfs of a node
+		return this.dfs; // outputs it as a string
+	}
+
+	public String printEdges() {
+
+		// string to output, required for GUI component
+		String edges = "";
+		
+		// runs for every node in the graph
+		for (Node node : adjacencyMap.keySet()) {
+
+			edges += ("Node " + node.name + " has an edge towards: "); // outputs to the console which node is
+																				// being looked at
+
+			// runs for all the neighbors of the given node
+			for (Node neighbor : adjacencyMap.get(node)) {
+
+				edges += (neighbor.name + " "); // outputs to the console the nodes connected to the current node
+														// in question
+			}
+
+			edges += "\n"; // clears the line for the next node to be displayed
+		}
+
+		return edges; // outputs the string of edges
+	}
+
+}
--- SE2205-GUI_Assignment/src/sample/Main.java
+++ SE2205-GUI_Assignment/src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.util.LinkedList;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
@@ -9,6 +11,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+//import 
 
 // How to create a basic JavaFX window:
 // 1. Create Stage (Window)
@@ -22,168 +26,181 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+	static Graph graph = new Graph();
+	static int counter = 0;
+	static LinkedList<Node> nodes = new LinkedList<Node>();
 
-        // Creating a GridPane object (layout)
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        // Setting the padding around the window - 10 pixels all around
-        grid.setPadding(new Insets(10,10,10,10));
+		// Creating a GridPane object (layout)
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
 
-        // Creating Labels
-        Label selectionLabel = new Label("Select your algorithm");
+		// Setting the padding around the window - 10 pixels all around
+		grid.setPadding(new Insets(10, 10, 10, 10));
 
-        // Creating radio buttons for DFS and BFS
-        ToggleGroup group = new ToggleGroup();
-        RadioButton dfs = new RadioButton("DFS");
-        RadioButton bfs = new RadioButton("BFS");
-        dfs.setToggleGroup(group);
-        bfs.setToggleGroup(group);
+		// Creating Labels
+		Label selectionLabel = new Label("Select your algorithm");
 
-        // Creating "Results" Label
-        Label resultsLabel = new Label("Results");
+		// Creating radio buttons for DFS and BFS
+		ToggleGroup group = new ToggleGroup();
+		RadioButton dfs = new RadioButton("DFS");
+		RadioButton bfs = new RadioButton("BFS");
+		dfs.setToggleGroup(group);
+		bfs.setToggleGroup(group);
 
-        // Creating "Run" button
-        Button runButton = new Button();
-        runButton.setText("Run");
-        runButton.setOnAction(e -> {
-            if (dfs.isSelected()){
-                ////////////////////////////////
-                // Code when DFS is selected ///
-                ////////////////////////////////
-            }else{
-                ////////////////////////////////
-                // Code when BFS is selected ///
-                ////////////////////////////////
-            }
-        });
+		// Creating "Results" Label
+		Label resultsLabel = new Label("Results");
 
-        // Creating "Add Node" button
-        Button addNodeButton = new Button();
-        addNodeButton.setText("Add Node");
-        addNodeButton.setOnAction(e -> {
-            addNodeWindow();
-        });
+		// Creating "Run" button
+		Button runButton = new Button();
+		runButton.setText("Run");
+		runButton.setOnAction(e -> {
+			if (dfs.isSelected()) {
 
-        // Creating "Add Edge" button
-        Button addEdgeButton = new Button();
-        addEdgeButton.setText("Add Edge");
-        addEdgeButton.setOnAction(e -> {
-            addEdgeWindow();
-        });
+				graph.resetNodesVisited(); // resets the list of nodes that have been visited
+				resultsLabel.setText(graph.DFSforGUI(nodes.getFirst())); // sets the label to the result of the DFS
 
-        // Creating "Print Edges" button
-        Label outputResultsLabel = new Label();
-        Button printEdgeButton = new Button();
-        printEdgeButton.setText("Print Edges");
-        printEdgeButton.setOnAction(e -> {
-            outputResultsLabel.setText("Place results here!");
-            //////////////////////////////////////
-            // Place code here to print results///
-            //////////////////////////////////////
-        });
+			} else {
 
-        // Constraining GUI components to grid
-        grid.setConstraints(selectionLabel, 0,0);
-        grid.setConstraints(dfs, 0,1);
-        grid.setConstraints(bfs, 1,1);
-        grid.setConstraints(runButton, 0,2);
-        grid.setConstraints(addNodeButton, 8,0);
-        grid.setConstraints(addEdgeButton, 8,1);
-        grid.setConstraints(printEdgeButton, 8,2);
-        grid.setConstraints(resultsLabel, 0,5);
-        grid.setConstraints(outputResultsLabel, 0,6);
+				graph.resetNodesVisited(); // resets the list of nodes that have been visited
+				resultsLabel.setText(graph.BFS(nodes.getFirst())); // sets the label to the result of the BFS
+			}
+		});
 
+		// Creating "Add Node" button
+		Button addNodeButton = new Button();
+		addNodeButton.setText("Add Node");
+		addNodeButton.setOnAction(e -> {
+			addNodeWindow();
+		});
 
-        // Adding components to grid
-        grid.getChildren().addAll(selectionLabel,dfs,bfs,runButton,addNodeButton,addEdgeButton,printEdgeButton,
-                resultsLabel,outputResultsLabel);
+		// Creating "Add Edge" button
+		Button addEdgeButton = new Button();
+		addEdgeButton.setText("Add Edge");
+		addEdgeButton.setOnAction(e -> {
+			addEdgeWindow();
+		});
 
-        Scene homeScene = new Scene(grid, 400,400);
-        primaryStage.setTitle("Graph");
-        primaryStage.setScene(homeScene);
-        primaryStage.show();
-    }
+		// Creating "Print Edges" button
+		Label outputResultsLabel = new Label();
+		Button printEdgeButton = new Button();
+		printEdgeButton.setText("Print Edges");
+		printEdgeButton.setOnAction(e -> {
+			outputResultsLabel.setText(graph.printEdges()); // prints the edges to the screen
 
-    private static void addEdgeWindow(){
-        Stage window = new Stage();
-        window.setTitle("Add Edge");
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(10);
-        grid.setHgap(10);
+		});
 
-        // Creating Labels
-        Label sourceLabel = new Label("Source");
-        Label destinationLabel = new Label("Destination");
+		// Constraining GUI components to grid
+		grid.setConstraints(selectionLabel, 0, 0);
+		grid.setConstraints(dfs, 0, 1);
+		grid.setConstraints(bfs, 1, 1);
+		grid.setConstraints(runButton, 0, 2);
+		grid.setConstraints(addNodeButton, 8, 0);
+		grid.setConstraints(addEdgeButton, 8, 1);
+		grid.setConstraints(printEdgeButton, 8, 2);
+		grid.setConstraints(resultsLabel, 0, 5);
+		grid.setConstraints(outputResultsLabel, 0, 6);
 
-        // Creating Text Fields
-        TextField sourceField = new TextField();
-            // sourceField.getText() to get text entered by the user
-        TextField destinationField = new TextField();
-            // destinationField.getText() to get text entered by the user
+		// Adding components to grid
+		grid.getChildren().addAll(selectionLabel, dfs, bfs, runButton, addNodeButton, addEdgeButton, printEdgeButton,
+				resultsLabel, outputResultsLabel);
 
-        // Creating "Add" button
-        Button addButton = new Button("Add");
-        addButton.setOnAction(e -> {
-            //////////////////////////////////
-            // Place code here to add nodes///
-            //////////////////////////////////
-        });
+		Scene homeScene = new Scene(grid, 400, 400);
+		primaryStage.setTitle("Graph");
+		primaryStage.setScene(homeScene);
+		primaryStage.show();
+	}
 
-        grid.getChildren().addAll(sourceLabel,destinationLabel,sourceField,destinationField,addButton);
+	private static void addEdgeWindow() {
+		Stage window = new Stage();
+		window.setTitle("Add Edge");
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(10);
+		grid.setHgap(10);
 
-        grid.setConstraints(sourceLabel, 0,0);
-        grid.setConstraints(destinationLabel, 0,1);
-        grid.setConstraints(sourceField, 3,0);
-        grid.setConstraints(destinationField, 3,1);
-        grid.setConstraints(addButton, 1,2);
+		// Creating Labels
+		Label sourceLabel = new Label("Source");
+		Label destinationLabel = new Label("Destination");
 
-        Scene addNodeScene = new Scene(grid);
-        window.setScene(addNodeScene);
-        window.show();
-    }
+		// Creating Text Fields
+		TextField sourceField = new TextField();
+		// sourceField.getText() to get text entered by the user
+		TextField destinationField = new TextField();
+		// destinationField.getText() to get text entered by the user
 
-    private static void addNodeWindow(){
-        Stage window = new Stage();
-        window.setTitle("Add Node");
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(10);
-        grid.setHgap(10);
+		// Creating "Add" button
+		Button addButton = new Button("Add");
+		addButton.setOnAction(e -> {
 
-        // Creating Labels
-        Label nodeNameLabel = new Label("Node Name");
+			Node source = null;
+			Node destination = null;
 
-        // Creating Text Fields
-        TextField nodeNameField = new TextField();
-            // nodeNameField.getText() to get text entered by the user
+			for (int i = 0; i < nodes.size(); i++) {
+				if (nodes.get(i).name.equals(sourceField.getText())) {
+					source = nodes.get(i);
+				}
+				if (nodes.get(i).name.equals(destinationField.getText())) {
+					destination = nodes.get(i);
+				}
+			}
+			graph.addEdge(source, destination);
+		});
 
-        // Creating "Add" button
-        Button addButton = new Button("Add");
-        addButton.setOnAction(e -> {
-            ////////////////////////////////////////
-            // Place code here to add nodes names///
-            ////////////////////////////////////////
-        });
+		grid.getChildren().addAll(sourceLabel, destinationLabel, sourceField, destinationField, addButton);
 
-        grid.getChildren().addAll(nodeNameLabel,nodeNameField,addButton);
+		grid.setConstraints(sourceLabel, 0, 0);
+		grid.setConstraints(destinationLabel, 0, 1);
+		grid.setConstraints(sourceField, 3, 0);
+		grid.setConstraints(destinationField, 3, 1);
+		grid.setConstraints(addButton, 1, 2);
 
-        grid.setConstraints(nodeNameLabel, 0,0);
-        grid.setConstraints(nodeNameField, 3,0);
-        grid.setConstraints(addButton, 1,2);
+		Scene addNodeScene = new Scene(grid);
+		window.setScene(addNodeScene);
+		window.show();
+	}
 
-        grid.getChildren().addAll();
-        Scene addEdgeScene = new Scene(grid);
-        window.setScene(addEdgeScene);
-        window.show();
-    }
+	private static void addNodeWindow() {
+		Stage window = new Stage();
+		window.setTitle("Add Node");
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(10);
+		grid.setHgap(10);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+		// Creating Labels
+		Label nodeNameLabel = new Label("Node Name");
+
+		// Creating Text Fields
+		TextField nodeNameField = new TextField();
+		// nodeNameField.getText() to get text entered by the user
+
+		// Creating "Add" button
+		Button addButton = new Button("Add");
+		addButton.setOnAction(e -> {
+
+			Node tempnode = new Node(counter, nodeNameField.getText());
+			counter++;
+			nodes.add(tempnode);
+		});
+
+		grid.getChildren().addAll(nodeNameLabel, nodeNameField, addButton);
+
+		grid.setConstraints(nodeNameLabel, 0, 0);
+		grid.setConstraints(nodeNameField, 3, 0);
+		grid.setConstraints(addButton, 1, 2);
+
+		grid.getChildren().addAll();
+		Scene addEdgeScene = new Scene(grid);
+		window.setScene(addEdgeScene);
+		window.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
